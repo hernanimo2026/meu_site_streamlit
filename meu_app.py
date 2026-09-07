635c5717b8b9601ae0479059a03a87aa270d9e0e
hernanimo2026 <hernanimo1972@gmail.com>
2026-09-07 09:28:23 -0400

    Add files via upload

--- meu_app.py
+++ meu_app.py
@@ -15,7 +15,7 @@ st.title("🧮 CALCULADORA DO PEDREIRO")
 col2_esq, col2_dir = st.columns(2)
 with col2_esq:
     st.write("Sistemas de Orçamentos Rápidos — Serviços Gerais")
-    st.write("Mensagem do dia? [Clique aqui] (https://youtu.be/f0x-BL8MkHc?si=SJXu-Lv-MhLEaFse)")
+    st.write("Mensagem do dia? [Clique aqui] (https://vt.tiktok.com/ZSqM1je11/)")
 with col2_dir:
     st.link_button("WhatsApp do pedreiro", "https://wa.me/5591991211780?text=Olá!%20Vim%20pelo%20site%20e%20gostaria%20de%20um%20orçamento.")
 
